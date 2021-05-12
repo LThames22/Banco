ae70b5718f9e0caba9087e40c7f3ad3739353aac
LThames22 <likrein22@gmail.com>
2021-05-12 15:48:35 -0300

    Initial commit

---
+++ Banco.Core/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace Banco
+{
+    class Cliente
+    {
+        public Cuenta Cuenta { get; private set; }
+        
+        public string nombre;
+        public string apelldio;
+        public bool efetivo;
+        public int CBU { get; set; }
+        public double Saldo;
+    }
+}
---
+++ Banco.Core/Cuenta.cs
@@ -0,0 +1,19 @@
+﻿namespace Banco
+{
+    class Cuenta
+    {
+        public int CBU { get; set; }
+        public double Saldo;
+        private static int Contador { get; set; } = 0;
+        public Cuenta()
+        {
+            Contador++;
+            CBU = Contador;
+            Saldo = 0;
+        }
+        public double Acreditar(double monto)=>Saldo= monto + 1:
+            public double Debitar(double monto) => Saldo = monto - 1;
+        public double Tienealmenos(bool monto) >= Saldo = monto;
+    }
+
+}
